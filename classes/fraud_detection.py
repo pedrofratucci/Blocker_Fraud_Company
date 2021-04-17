8acda948a04d7723a995d182f930849dcf2ff0c3
pedrofratucci <pedrofratucci8@hotmail.com>
2021-04-17 13:54:15 -0300

    First commit

--- classes/fraud_detection.py
+++ classes/fraud_detection.py
@@ -15,34 +15,44 @@ class Fraud_Detection(object):
         self.new_balance_dest_scaler = pickle.load(open('parameters/new_balance_dest_scaler.pkl', 'rb'))
         self.new_balance_orig_scaler = pickle.load(open('parameters/new_balance_orig_scaler.pkl', 'rb'))
         self.step_scaler = pickle.load(open('parameters/step_scaler.pkl', 'rb'))
+        self.ohe = pickle.load(open('parameters/onehotencoder_amount_level.pkl', 'rb'))
         self.manual_selected_features = pickle.load(open('parameters/manual_selected_features.pkl', 'rb'))
 
     def features_engineering(self, df):
 
-        '''create and transform features'''
+        '''creating and transforming dataset's features'''
 
-        # create a dataset's column with the day
+        # creating the dataset's new columns names
+        columns = ['step', 'type', 'amount', 'nameOrig', 'OldBalanceOrig', 'NewBalanceOrig',
+                   'nameDest', 'oldBalanceDest', 'NewBalanceDest', 'IsFraud', 'IsFlaggedFraud']
+
+        # changing the dataset's columns names for the 'new_columns' list values
+        snakecase = lambda x: inflection.underscore(x)
+        new_columns = list(map(snakecase, columns))
+        df1.columns = new_columns
+
+        # creating a dataset's column with the day
         df['day'] = df['step'].apply(lambda x: int(x / 24 + 1))
 
-        # create a dataset's column with the day of the week type (weekend or workweek)
+        # creating a dataset's column with the day of the week type (weekend or workweek)
         df['weekend'] = df['day'].apply(lambda x:
                                         1 if x in [6, 7, 13, 14, 20, 21, 27, 28] else
                                         0)
 
-        # create a dataset's column with difference (amount) between the old and new origin account balance
+        # creating a dataset's column with difference (amount) between the old and new origin account balance
         df['flow_orig'] = df['new_balance_orig'] - df['old_balance_orig']
 
-        # create a dataset's column with difference (amount) between the old and new destination account balance
+        # creating a dataset's column with difference (amount) between the old and new destination account balance
         df['flow_dest'] = df['new_balance_dest'] - df['old_balance_dest']
 
-        # reset the dataset's original index to 0
+        # resetting the dataset's original index to 0
         df.reset_index(drop=True, inplace=True)
 
         return df
 
     def data_preparation(self, df):
 
-        ''' prepare the dataset features '''
+        ''' preparing the dataset's features '''
 
         # rescaling numerical features
         df['amount'] = self.amount_scaler.transform(df[['amount']].values)
@@ -52,31 +62,13 @@ class Fraud_Detection(object):
         df['new_balance_orig'] = self.new_balance_orig_scaler.transform(df[['new_balance_orig']].values)
         df['step'] = self.step_scaler.transform(df[['step']].values)
 
-        # delete the is_fraud column, because it is the answer and it will bug the OneHotEncoder method it it remains in the dataset
+        # deleting the is_fraud column, because it is the answer and it will bug the OneHotEncoder method it it remains in the dataset
         df.drop(columns=['is_fraud'], inplace=True)
 
-        # instantiate the OneHotEncoder method as 'ohe'
-        ohe = OneHotEncoder(cols=['type'], use_cat_names=True)
+        # transforming the 'df' dataset with the 'ohe' method
+        df = self.ohe.transform(df)
 
-        # transform the 'df' dataset with the 'ohe' method
-        df = ohe.fit_transform(df)
-
-        # create a list with the dataset columns name
-        colunas = list(df.columns)
-
-        # create the new columns that the 'ohe' method didn't created, and supposed to
-        if 'type_CASH_OUT' not in colunas:
-            df['type_CASH_OUT'] = 0
-        if 'type_TRANSFER' not in colunas:
-            df['type_TRANSFER'] = 0
-        if 'type_CASH_IN' not in colunas:
-            df['type_CASH_IN'] = 0
-        if 'type_DEBIT' not in colunas:
-            df['type_DEBIT'] = 0
-        if 'type_PAYMENT' not in colunas:
-            df['type_PAYMENT'] = 0
-
-        # rename the columns created by the OneHotEncoder and manually methods
+        # renaming the columns created by the OneHotEncoder method
         df.rename(columns={'type_CASH_OUT': 'cash_out',
                            'type_PAYMENT': 'payment',
                            'type_CASH_IN': 'cash_in',
@@ -90,15 +82,15 @@ class Fraud_Detection(object):
 
         ''' Filtering the features that will be used to predict and predict '''
 
-        # select only the features that will be used to predict the result
+        # selecting only the features that will be used to predict the result
         selected_features = self.manual_selected_features
 
         df_copy = df[selected_features].copy()
 
-        # get prediction
+        # getting prediction
         df_copy['predict'] = model.predict(df_copy)
 
-        # manipulate the return
+        # manipulating the return
         if df_copy['predict'][0] == 1:
             df_copy['answer'] = 'This is a fraudulent transaction'
         if df_copy['predict'][0] == 0:
